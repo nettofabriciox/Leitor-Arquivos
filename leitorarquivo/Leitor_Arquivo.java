bd84043721866a58c0e5244301f50efdf7819f24
nettofabriciox <agostinhonetto73>
2023-10-21 17:21:59 -0300

    Segundo Commit - BufferedReader

--- leitorarquivo/Leitor_Arquivo.java
+++ leitorarquivo/Leitor_Arquivo.java
@@ -1,29 +1,26 @@
 package leitorarquivo;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Leitor_Arquivo {
 
 	public static void main(String[] args) {
-	
-		File caminho = new File("C:\\Users\\agost\\OneDrive\\Documentos\\in.txt");
-        Scanner sc = null;
-        try {
-        	sc = new Scanner(caminho);
-        	while(sc.hasNextLine()) {
-        		System.out.println(sc.nextLine());
-        	}
-        }
-        catch(IOException e) {
-        	System.out.println("Error: " + e.getMessage());
-        }
-        finally {
-        	if(sc != null) {
-        		sc.close();
-        	}
-        }
-	}
 
+		String caminho = "C:\\Users\\agost\\OneDrive\\Documentos\\in.txt";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+
+			String linha = br.readLine();
+			while (linha != null) {
+				System.out.println(linha);
+				linha = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
 }
